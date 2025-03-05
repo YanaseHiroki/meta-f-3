3335d74a3719f854e0cc02a432981bdb24fe4ddf
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2025-03-06 01:39:21 +0900

    Create difyChatFlowApi.js

---
+++ difyChatFlowApi.js
@@ -0,0 +1,70 @@
+// Difyの「1~5個の広告を分析するCF」にAPIで接続する
+// DifyのワークフローにAPIで接続する
+// 接続先：1つの広告を分析するWF
+function difyChatflowApi() {
+    const headers = {
+        'Authorization': "Bearer app-ROciu3Dz5jcuIPAOAEUQNGTe",  //api key発行してペースト
+        'Content-Type': 'application/json'              //必須
+    };
+    
+    // Extracting additional parameters from the sheet
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CRTレポート");
+    const data = sheet.getRange("B5:N9").getValues(); // B5:N9の範囲を取得（最大5行）
+    let addArr = [];
+    let imagesForPayload = [];
+
+    data.forEach(row => {
+        if (row[0]) { // B列に値がある場合
+            addArr.push({
+                'ad_name': row[0],
+                'spend': row[4],
+                'impression': row[5],
+                'cpm': row[6],
+                'click': row[7],
+                'ctr': row[8],
+                'cpc': row[9],
+                'conversion': row[10],
+                'cvr': row[11],
+                'cpa': row[12]
+            });
+            imagesForPayload.push({
+                "type": "image",
+                "transfer_method": "remote_url",
+                "url": row[1]
+            });
+        }
+    });
+
+    const payload = JSON.stringify({
+        "user": "gas-difyChatflowApi",                            // 任意の文字列で可能（監視で表示される）
+        'response_mode': 'streaming',
+        'inputs': {
+            "adds": addArr,
+            "image_urls": imgUrlArr.join(",")
+        },
+        'query': 'この広告を分析してください。',
+        'files': imagesForPayload
+    });
+
+    const options = {
+        "method": "post",
+        "payload": payload,
+        "headers": headers,
+        "muteHttpExceptions": true                      // エラーを平文で返してもらう
+    };
+
+    const requestUrl = "https://api.dify.ai/v1/chatflows/run";
+    const response = UrlFetchApp.fetch(requestUrl, options);
+    const responseText = response.getContentText();
+    const responseJson = JSON.parse(responseText);
+
+    // 帰ってきたレスポンスを表示
+    Logger.log("responseText: " + responseText);
+    
+    // StatusCodeによって処理分岐
+    if (response.getResponseCode() === 200) {
+        Logger.log('responseJson.data.outputs.text: ' + responseJson.data.outputs.text); // レスポンスのdata部分をログ出力
+    } else {
+        Logger.log("Error"); // エラー発生時のログ出力
+    }
+}
