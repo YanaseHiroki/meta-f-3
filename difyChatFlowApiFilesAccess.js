5cee1371f58c119f3bf6ba5305c318eaf9ca8483
Hiroki Yanase <mamorukun7@gmail.com>
2025-03-07 17:39:46 +0900

    Dify API接続機能を追加し、広告分析用のデータ処理を実装

---
+++ difyChatFlowApiFilesAccess.js
@@ -0,0 +1,83 @@
+// Difyの「1~5個の広告を分析するCF (access url)」にAPIで接続する
+// DifyのワークフローにAPIで接続する
+// 接続先：1つの広告を分析するWF
+function difyChatflowApiFilesAccess() {
+
+    // ヘッダー情報
+    const headers = {
+        'Authorization': "Bearer app-R2f9xzr4VeznYFxX4fagkQGl",  //api key　(1~5個の広告を分析するCF (access url))
+        'Content-Type': 'application/json'              //必須
+    };
+
+    // Extracting additional parameters from the sheet
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("CRTレポート");
+    const data = sheet.getRange("B5:B9").getValues(); // B5:N9の範囲を取得（最大5行）
+
+    let addArr = [];
+    let imagesForPayload = [];
+
+    data.forEach(row => {
+        if (row[0]) { // B列に値がある場合
+            addArr.push({
+                'ad_name': row[0],
+                'image_url': row[1],
+                'spend': row[4],
+                'impression': row[5],
+                'cpm': row[6],
+                'click': row[7],
+                'ctr': row[8],
+                'cpc': row[9],
+                'conversion': row[10],
+                'cvr': row[11],
+                'cpa': row[12]
+            });
+            Logger.log('addArr: ' + JSON.stringify(addArr));
+            imagesForPayload.push({
+                "type": "image",
+                "transfer_method": "remote_url",
+                "url": row[1]
+            });
+            Logger.log('imagesForPayload: ' + JSON.stringify(imagesForPayload));
+        }
+    });
+
+    const addsForPayloard = JSON.stringify(addArr);
+    Logger.log('addsForPayloard: ' + addsForPayloard);
+
+    const payload = JSON.stringify({
+        "user": "gas-difyChatflowApi",
+        'response_mode': 'blocking',
+        'inputs': {
+            "adds": addsForPayloard,
+        },
+        'query': 'この広告を分析してください。',
+        'files': imagesForPayload
+    });
+
+    const options = {
+        "method": "post",
+        "payload": payload,
+        "headers": headers,
+        "muteHttpExceptions": true
+    };
+
+    const requestUrl = "https://api.dify.ai/v1/chat-messages";
+    const response = UrlFetchApp.fetch(requestUrl, options);
+    const responseText = response.getContentText();
+    const responseJson = JSON.parse(responseText);
+
+    // 帰ってきたレスポンスを表示
+    Logger.log("responseText: " + responseText);
+    
+    // StatusCodeによって処理分岐
+    if (response.getResponseCode() === 200) {
+        const answerJson = JSON.parse(responseJson.answer);
+        Logger.log('現状整理: ' + answerJson.current_status);
+        Logger.log('今後の示唆: ' + answerJson.future_implications);
+        Logger.log('画像情報: ' + answerJson.img_info);
+        console.log('difyChatflowApi return answerJson: ' + JSON.stringify(answerJson));
+        return answerJson;
+    } else {
+        Logger.log("difyChatflowApi Error");
+    }
+}
